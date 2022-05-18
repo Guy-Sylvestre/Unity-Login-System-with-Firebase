edda83328d3f1075ad5ea1e340d914aab253c891
unknown <guysylvestre876@gmail.com>
2022-05-18 18:51:08 +0000

    3-Unity Login System with Firebase Authentication _ Import & Implement Firebase SD

--- Assets/Scripts/FirebaseController.cs
+++ Assets/Scripts/FirebaseController.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using System;
+using System.Threading.Tasks;
+using Firebase.Extensions;
+
 
 public class FirebaseController : MonoBehaviour
 {
@@ -10,8 +16,34 @@ public class FirebaseController : MonoBehaviour
     public Text notif_Title_Text, notif_Message_Text, profileUserName_Text, profileUserEmail_Text;
     public Toggle rememberMe;
 
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+
+    bool isSignIn = false;
+
+
+    void Start()
+    {
+       Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available) {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                InitializeFirebase();
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            } else {
+                UnityEngine.Debug.LogError(System.String.Format(
+                "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+       
+    }
+
     public void OpenLoginPanel()
     {
+        // Acces a la fenetre de connexion
         loginPanel.SetActive(true);
         signupPanel.SetActive(false);
         profilePanel.SetActive(false);
@@ -20,6 +52,7 @@ public class FirebaseController : MonoBehaviour
 
     public void OpenSignupPanel()
     {
+        // Acces a la fenetre d'inscription
         loginPanel.SetActive(false);
         signupPanel.SetActive(true);
         profilePanel.SetActive(false);
@@ -28,6 +61,7 @@ public class FirebaseController : MonoBehaviour
 
     public void OpenProfilePanel()
     {
+        // Acces a la fenetre du profile de l'utilisateur
         loginPanel.SetActive(false);
         signupPanel.SetActive(false);
         profilePanel.SetActive(true);
@@ -36,6 +70,7 @@ public class FirebaseController : MonoBehaviour
 
     public void OpenForgetPassPanel()
     {
+        // Acces a la fenetre du mot de passe oublier
         loginPanel.SetActive(false);
         signupPanel.SetActive(false);
         profilePanel.SetActive(false);
@@ -44,6 +79,7 @@ public class FirebaseController : MonoBehaviour
 
     public void LoginUser()
     {
+        // Validation des champs de connexion pour la page de connexion
         if (string.IsNullOrEmpty(LoginEmail.text) && string.IsNullOrEmpty(loginPassword.text))
         {
             showNotificationMessage("Error", "Fields Empty! Please Input Details In All Fields");
@@ -51,9 +87,11 @@ public class FirebaseController : MonoBehaviour
         }
 
         // Do login
+        SignInUser(LoginEmail.text, loginPassword.text);
     }
 
     public void SignUpUser(){
+        // Validation des champs de d'inscription pour la page d'inscription
         if (string.IsNullOrEmpty(signupEmail.text) && string.IsNullOrEmpty(signupPassword.text) && string.IsNullOrEmpty(signupConfirmPassword.text))
         {
             showNotificationMessage("Error", "Fields Empty! Please Input Details In All Fields");
@@ -61,20 +99,22 @@ public class FirebaseController : MonoBehaviour
         }
 
         // Do SignUp
+        CreateUser(signupEmail.text, signupPassword.text, signupUserName.text);
     }
 
     public void forgetPass(){
+        // Validation du champs du mail pour la page de mot de passe oublier
         if (string.IsNullOrEmpty(forgetPassEmail.text))
         {
             showNotificationMessage("Error", "Fields Empty! Please Input Details In All Fields");
             return;
         }
-
-        // Do SignUp
+        
     }
 
     private void showNotificationMessage(string title, string message)
     {
+        // Declenchement de l'alert du message d'error quand aucun champ n'est rempli
         notif_Title_Text.text = "" + title;
         notif_Message_Text.text = "" + message;
 
@@ -83,6 +123,7 @@ public class FirebaseController : MonoBehaviour
 
     public void CloseNotification_Panel()
     {
+        // Fermer la fenettre du message d'error
         notif_Title_Text.text = "";
         notif_Message_Text.text = "";
 
@@ -91,11 +132,133 @@ public class FirebaseController : MonoBehaviour
 
     public void LogOut()
     {
+        // Systeme de econnexion
         // profilePanel.SetActive(false);
+        auth.SignOut();
         profileUserEmail_Text.text = "";
         profileUserName_Text.text = "";
         OpenLoginPanel();
     }
+
+
+    // Sign up new users
+    void CreateUser(string email, string password, string Username)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            UpdateUserProfile(Username);
+        });
+    }
+
+
+    // Sign in existing users
+    public void SignInUser(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            profileUserName_Text.text = "" + newUser.DisplayName;
+            profileUserEmail_Text.text = "" + newUser.Email;
+             
+            OpenProfilePanel();
+        });
+    }
+
+
+    // Set an authentication state change event handler and get user data
+    void InitializeFirebase() {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+        }
+
+        void AuthStateChanged(object sender, System.EventArgs eventArgs) {
+        if (auth.CurrentUser != user) {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null) {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn) {
+                Debug.Log("Signed in " + user.UserId);
+                isSignIn = true;
+            }
+        }
+    }
+
+
+    // Deconnexion
+    void OnDestroy() {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+
+
+    // Update a user's profile
+    void UpdateUserProfile(string UserName)
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null) {
+            Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile {
+                DisplayName = UserName,
+                PhotoUrl = new System.Uri("https://via.placeholder.com/150"),
+            };
+        user.UpdateUserProfileAsync(profile).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("UpdateUserProfileAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("UpdateUserProfileAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Debug.Log("User profile updated successfully.");
+            showNotificationMessage("Alert", "Account Successfully Created");
+        });
+        }
+    }
+
+
+    bool isSigned = false;
+
+    void Update()
+    {
+        if (isSignIn)
+        {
+            if (!isSigned)
+            {
+                isSigned = true;
+                profileUserName_Text.text = "" + user.DisplayName;
+                profileUserEmail_Text.text = "" + user.Email;
+
+                OpenProfilePanel();
+            }
+        }
+    }
 }
 
-// 18MIN09S
